ebe151c9a5a64d89d555188e34f9aaa649b6b567
larisalungu6 <35381620+larisalungu6@users.noreply.github.com>
2018-01-12 20:23:52 +0000

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PicBullet = New System.Windows.Forms.PictureBox()
+        Me.Spaceship = New System.Windows.Forms.PictureBox()
+        Me.PicInvader1 = New System.Windows.Forms.PictureBox()
+        Me.PicInvader3 = New System.Windows.Forms.PictureBox()
+        Me.PicInvader2 = New System.Windows.Forms.PictureBox()
+        Me.PicInvader4 = New System.Windows.Forms.PictureBox()
+        Me.PicInvader5 = New System.Windows.Forms.PictureBox()
+        Me.PicInvader6 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PicBullet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Spaceship, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicInvader1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicInvader3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicInvader2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicInvader4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicInvader5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicInvader6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'PicBullet
+        '
+        Me.PicBullet.Image = Global.WindowsApplication2.My.Resources.Resources.Capture
+        Me.PicBullet.Location = New System.Drawing.Point(249, 275)
+        Me.PicBullet.Name = "PicBullet"
+        Me.PicBullet.Size = New System.Drawing.Size(36, 27)
+        Me.PicBullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBullet.TabIndex = 7
+        Me.PicBullet.TabStop = False
+        '
+        'Spaceship
+        '
+        Me.Spaceship.Image = Global.WindowsApplication2.My.Resources.Resources.alienspaceship
+        Me.Spaceship.Location = New System.Drawing.Point(240, 308)
+        Me.Spaceship.Name = "Spaceship"
+        Me.Spaceship.Size = New System.Drawing.Size(61, 52)
+        Me.Spaceship.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Spaceship.TabIndex = 6
+        Me.Spaceship.TabStop = False
+        '
+        'PicInvader1
+        '
+        Me.PicInvader1.Image = Global.WindowsApplication2.My.Resources.Resources.Virus_Emoji_large
+        Me.PicInvader1.Location = New System.Drawing.Point(240, 155)
+        Me.PicInvader1.Name = "PicInvader1"
+        Me.PicInvader1.Size = New System.Drawing.Size(45, 41)
+        Me.PicInvader1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicInvader1.TabIndex = 5
+        Me.PicInvader1.TabStop = False
+        '
+        'PicInvader3
+        '
+        Me.PicInvader3.Image = Global.WindowsApplication2.My.Resources.Resources.Virus_Emoji_large
+        Me.PicInvader3.Location = New System.Drawing.Point(179, 92)
+        Me.PicInvader3.Name = "PicInvader3"
+        Me.PicInvader3.Size = New System.Drawing.Size(43, 43)
+        Me.PicInvader3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicInvader3.TabIndex = 4
+        Me.PicInvader3.TabStop = False
+        '
+        'PicInvader2
+        '
+        Me.PicInvader2.Image = Global.WindowsApplication2.My.Resources.Resources.Virus_Emoji_large
+        Me.PicInvader2.Location = New System.Drawing.Point(303, 92)
+        Me.PicInvader2.Name = "PicInvader2"
+        Me.PicInvader2.Size = New System.Drawing.Size(42, 43)
+        Me.PicInvader2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicInvader2.TabIndex = 3
+        Me.PicInvader2.TabStop = False
+        '
+        'PicInvader4
+        '
+        Me.PicInvader4.Image = Global.WindowsApplication2.My.Resources.Resources.Virus_Emoji_large
+        Me.PicInvader4.Location = New System.Drawing.Point(377, 26)
+        Me.PicInvader4.Name = "PicInvader4"
+        Me.PicInvader4.Size = New System.Drawing.Size(41, 40)
+        Me.PicInvader4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicInvader4.TabIndex = 2
+        Me.PicInvader4.TabStop = False
+        '
+        'PicInvader5
+        '
+        Me.PicInvader5.Image = Global.WindowsApplication2.My.Resources.Resources.Virus_Emoji_large
+        Me.PicInvader5.Location = New System.Drawing.Point(240, 26)
+        Me.PicInvader5.Name = "PicInvader5"
+        Me.PicInvader5.Size = New System.Drawing.Size(45, 40)
+        Me.PicInvader5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicInvader5.TabIndex = 1
+        Me.PicInvader5.TabStop = False
+        '
+        'PicInvader6
+        '
+        Me.PicInvader6.Image = Global.WindowsApplication2.My.Resources.Resources.Virus_Emoji_large
+        Me.PicInvader6.Location = New System.Drawing.Point(126, 26)
+        Me.PicInvader6.Name = "PicInvader6"
+        Me.PicInvader6.Size = New System.Drawing.Size(44, 40)
+        Me.PicInvader6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicInvader6.TabIndex = 0
+        Me.PicInvader6.TabStop = False
+        '
+        'Timer2
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(796, 420)
+        Me.Controls.Add(Me.PicBullet)
+        Me.Controls.Add(Me.Spaceship)
+        Me.Controls.Add(Me.PicInvader1)
+        Me.Controls.Add(Me.PicInvader3)
+        Me.Controls.Add(Me.PicInvader2)
+        Me.Controls.Add(Me.PicInvader4)
+        Me.Controls.Add(Me.PicInvader5)
+        Me.Controls.Add(Me.PicInvader6)
+        Me.KeyPreview = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PicBullet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Spaceship, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicInvader1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicInvader3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicInvader2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicInvader4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicInvader5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicInvader6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PicInvader6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicInvader5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicInvader4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicInvader2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicInvader3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicInvader1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Spaceship As System.Windows.Forms.PictureBox
+    Friend WithEvents PicBullet As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,118 @@
+﻿Public Class Form1
+
+    Dim LeftMove As Integer = 5
+    Dim Invader(6) As PictureBox
+    Dim invaderLife(6) As Boolean
+    Dim up_down_mover = 5
+    Dim Left_right_mover = 5
+
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles Me.KeyDown
+
+        Select Case e.KeyCode
+            Case Keys.Left
+
+                Left_right_mover = -5
+                up_down_mover = 0
+                direct = 1
+
+            Case Keys.Right
+                Left_right_mover = 5
+                up_down_mover = 0
+                direct = 2
+
+            Case Keys.Up
+                up_down_mover = -5
+                Left_right_mover = 0
+                direct = 3
+
+            Case Keys.Down
+                up_down_mover = 5
+                Left_right_mover = 0
+                direct = 4
+                Timer1.Start()
+            Case PicBullet.Location = New System.Drawing.Point(Spaceship.Left + ((Spaceship.Width) / 2), Spaceship.Top)
+                Timer2.Stop()
+        End Select
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+
+        Invader(1) = PicInvader1
+        Invader(2) = PicInvader2
+        Invader(3) = PicInvader3
+        Invader(4) = PicInvader4
+        Invader(5) = PicInvader5
+        Invader(6) = PicInvader6
+
+        For j = 1 To 6
+            invaderLife(j) = True
+        Next
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Spaceship.Top += up_down_mover
+        Spaceship.Left += Left_right_mover
+        For j = 1 To 6
+            If invaderLife(j) Then
+                Invader(j).Left += LeftMove
+                If Invader(j).Left > 1000 Then
+
+                    LeftMove *= -1
+                End If
+                If Invader(j).Left < 10 Then
+                    LeftMove *= -1
+                    Invader(j).Left = 11
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub Timer2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        PicBullet.Top -= 5
+        If PicBullet.Top <= 5 Then
+            Timer2.Stop()
+            PicBullet.Top = 500
+
+        End If
+        If PicBullet.Bounds.IntersectsWith(PicInvader1.Bounds) Then
+            PicInvader1.Location = New System.Drawing.Point(12, 500)
+            invaderLife(1) = False
+            PicBullet.Top = 500
+            Timer2.Stop()
+        End If
+        If PicBullet.Bounds.IntersectsWith(PicInvader2.Bounds) Then
+            PicInvader2.Location = New System.Drawing.Point(12, 500)
+            invaderLife(2) = False
+            PicBullet.Top = 500
+            Timer2.Stop()
+        End If
+        If PicBullet.Bounds.IntersectsWith(PicInvader3.Bounds) Then
+            PicInvader3.Location = New System.Drawing.Point(12, 500)
+            invaderLife(3) = False
+            PicBullet.Top = 500
+            Timer2.Stop()
+        End If
+        If PicBullet.Bounds.IntersectsWith(PicInvader4.Bounds) Then
+            PicInvader4.Location = New System.Drawing.Point(12, 500)
+            invaderLife(4) = False
+            PicBullet.Top = 500
+            Timer2.Stop()
+        End If
+        If PicBullet.Bounds.IntersectsWith(PicInvader5.Bounds) Then
+            PicInvader5.Location = New System.Drawing.Point(12, 500)
+            invaderLife(5) = False
+            PicBullet.Top = 500
+            Timer2.Stop()
+        End If
+        If PicBullet.Bounds.IntersectsWith(PicInvader6.Bounds) Then
+            PicInvader6.Location = New System.Drawing.Point(12, 500)
+            invaderLife(6) = False
+            PicBullet.Top = 500
+            Timer2.Stop()
+        End If
+    End Sub
+End Class
+
+
